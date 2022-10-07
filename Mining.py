89365ddf206d22ff9da595305a3f02744db89d5c
JanneSickert <janne.sickert@gmx.de>
2022-10-07 13:22:22 +0200

    Layout

---
+++ Mining.py
@@ -0,0 +1,56 @@
+import pandas as pd
+
+class SortData:
+    CSV_PATH = "supermarket_sales.csv"
+    df = None
+
+    @staticmethod
+    def init_dataframe():
+        SortData.df = pd.read_csv(SortData.CSV_PATH, sep=',')
+        month = []
+        monthAsString = []
+        for e in SortData.df["Datum"]:
+            r = e.split("/")[0]
+            if r == "1":
+                monthAsString.append("Jan")
+            elif r == "2":
+                monthAsString.append("Feb")
+            elif r == "3":
+                monthAsString.append("Mär")
+            else:
+                print("Error: Mehr als drei Mohnate")
+            month.append(r)
+        SortData.df["month"] = month
+        SortData.df["Month"] = monthAsString
+    
+    @staticmethod
+    def umsatz_nach_filialen():
+        grouped = SortData.df.groupby(["Filiale"]).sum()
+        return {
+            "Filiale": list(grouped["Gesamtpreis"].keys()),
+            "Gesamtpreis": list(grouped["Gesamtpreis"].values)
+        }
+    
+    @staticmethod
+    def umsatz_nach_geschlecht():
+        grouped = SortData.df.groupby(["Geschlecht"]).sum()
+        res = pd.DataFrame(grouped["Gesamtpreis"])
+        return res
+    
+    @staticmethod
+    def urzeit_umsatz_produktlinie():
+        zeit = SortData.df["Zeit"]
+        i = 0
+        while i < len(zeit):
+            zeit[i] = str(zeit[i]).split(":")[0]
+            i += 1
+        SortData.df["Zeit"] = zeit
+        grouped = SortData.df.groupby(["Zeit", "Produktlinie"]).sum()
+        res = pd.DataFrame(grouped["Gesamtpreis"].keys(), grouped["Gesamtpreis"].values)
+        return res
+    
+    @staticmethod
+    def produktlienie_und_bewertungen():
+        grouped = SortData.df.groupby(["Produktlinie"]).mean()
+        res = pd.DataFrame(grouped["Bewertung"])
+        return res
--- app.py
+++ app.py
@@ -1,43 +1,76 @@
+import dash
+import  dash_html_components as html
+import plotly.graph_objects as go
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
 import pandas as pd
-import plotly.express as px
+import dash_bootstrap_components as dbc
+import numpy as np
+import os
+import Mining as mg
 
-class SortData:
-    CSV_PATH = "supermarket_sales.csv"
-    df = None
+mg.SortData.init_dataframe()
+print(mg.SortData.df)
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+PLOTLY_LOGO = "https://images.plot.ly/logo/new-branding/plotly-logomark.png"
+TITLE = 'Supermarket Sales'
+app.title = TITLE
+server = app.server
+navbar = dbc.Navbar( id = 'navbar', children = [
+    dbc.Row([
+        dbc.Col(html.Img(src = PLOTLY_LOGO, height = "70px")),
+        dbc.Col(
+            dbc.NavbarBrand(TITLE, 
+            style = {'color':'white', 'fontSize':'25px','fontFamily':'Times New Roman'}))
+        ],align = "center")
+    ], color = '#090059')
+body_app = dbc.Container([
+    html.Br(),
+    html.Br(),
+    dbc.Row([
+        dbc.Col([dbc.Card([
+            dbc.CardBody([
+                    html.H6('Monate auswählen', style = {'textAlign':'center'}),
+                    dbc.Row([
+                        dbc.Col([
+                            html.H6('Aktueller Monat'),
+                            dcc.Dropdown( id = 'dropdown_base', options = [
+                            {'label':i, 'value':i } for i in ["Jan", "Feb", "Mär"]],
+                            value = 'Feb',)
+                        ]),
+                        dbc.Col([
+                            html.H6('Referenzmonat'),
+                            dcc.Dropdown( id = 'dropdown_comp', options = [
+                            {'label':i, 'value':i } for i in ["Jan", "Feb", "Mär"]],
+                            value = 'Jan',)
+                            ]
+                        ),
+                    ])
+                ]
+            )
+        ],
+        style={'height':'150px'})],width = 4),
+        dbc.Col([dbc.Card(id = 'card_num1',style={'height':'150px'})]),
+        dbc.Col([dbc.Card(id = 'card_num2',style={'height':'150px'})])
+    ]),
+    html.Br(),
+    html.Br(),
+    dbc.Row([
+        dbc.Col([dbc.Card(id = 'card_num3',style={'height':'350px'})]),
+        dbc.Col([dbc.Card(id = 'card_num4',style={'height':'350px'})])
+        ]),
+    html.Br(),
+    html.Br(),
+    dbc.Row([
+        dbc.Col([dbc.Card(id = 'card_num5',style={'height':'350px'})]),
+        dbc.Col([dbc.Card(id = 'card_num6',style={'height':'350px'})])
+        ]),
+    html.Br(),
+    html.Br()
+    ],
+    style = {'backgroundColor':'#f7f7f7'}, fluid = True
+    )
+app.layout = html.Div(id = 'parent', children = [navbar, body_app])
 
-    @staticmethod
-    def init_dataframe():
-        SortData.df = pd.read_csv(SortData.CSV_PATH, sep=',')
-    
-    @staticmethod
-    def umsatz_nach_filialen():
-        grouped = SortData.df.groupby(["Filiale"]).sum()
-        res = pd.DataFrame(grouped["Gesamtpreis"])
-        return res
-    
-    @staticmethod
-    def umsatz_nach_geschlecht():
-        grouped = SortData.df.groupby(["Geschlecht"]).sum()
-        res = pd.DataFrame(grouped["Gesamtpreis"])
-        return res
-    
-    @staticmethod
-    def urzeit_umsatz_produktlinie():
-        zeit = SortData.df["Zeit"]
-        i = 0
-        while i < len(zeit):
-            zeit[i] = str(zeit[i]).split(":")[0]
-            i += 1
-        SortData.df["Zeit"] = zeit
-        grouped = SortData.df.groupby(["Zeit", "Produktlinie"]).sum()
-        res = pd.DataFrame(grouped["Gesamtpreis"])
-        return res
-    
-    @staticmethod
-    def produktlienie_und_bewertungen():
-        grouped = SortData.df.groupby(["Produktlinie"]).mean()
-        res = pd.DataFrame(grouped["Bewertung"])
-        return res
-
-SortData.init_dataframe()
-print(SortData.urzeit_umsatz_produktlinie())
+if __name__ == "__main__":
+    app.run_server()
